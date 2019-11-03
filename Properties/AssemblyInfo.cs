5945d4e01fb1c779b54aea2d8fb755fe9e160360
PatrickC <pat31718@gmail.com>
2019-11-03 23:58:14 +0800

    Update AssemblyInfo.cs
    v1.0.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HTMLZoomTool")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019 OpenSource by PatrickC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
